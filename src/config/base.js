8143d0a1c9f593075169a0f4f2b551059eb943c6
Zak Zubair <zakaaf@hotmail.co.uk>
2018-02-01 11:18:11 +0000

    Initial commit 📣

---
+++ src/config/base.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Settings configured here will be merged into the final config object.
+export default {
+	ACCOUNT_KEY: '5krE6O7pyhn7X4X0vyvHs6v2aAv4ZpWO',
+	// Set to 'docked' or 'normal' for docked or normal mode respectively
+	THEME: 'normal'
+}
